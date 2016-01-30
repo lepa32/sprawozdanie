07192e76fa2b3e5a17b84dad5ca895011f9bf074
lepa32 <kamil_lepek@o2.pl>
2016-01-30 23:25:20 +0100

    program do sprawka

---
+++ sprawko.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout<<"Przykadowy program do sprawozdania na dodawanie dwóch liczb"<<endl;
+int a,b,suma;
+suma=a+b;
+
+cout<<"Suma = "<<suma<<endl;
+
+return 0;
+}
